101d41cd88a9a90f80f9ce779ec87e38d1001d10
ch <ckdgus1120@gmail.com>
2023-11-02 21:35:48 +0900

    1

--- omechu/omechu/Views/ChoiceView.swift
+++ omechu/omechu/Views/ChoiceView.swift
@@ -10,29 +10,41 @@ import SwiftUI
 struct ChoiceView: View {
     @Binding var foods: [Food]
     @Binding var selectedFoodIdx: Int?
-    
+    @State private var isShowingNewRoomView = false // State for showing NewRoomView
+
     var body: some View {
         if selectedFoodIdx != nil {
             VStack {
                 Spacer()
                 Text("\(foods[selectedFoodIdx!].name)")
                     .font(.headline)
-                Image(uiImage: UIImage(data: foods[selectedFoodIdx!].imgData)!) // 이미지 데이터로 이미지 생성
+                Image(uiImage: UIImage(data: foods[selectedFoodIdx!].imgData)!)
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 180, height: 200) // 이미지 크기 조절
+                    .frame(width: 180, height: 200)
                 Text("음식 선정 완료!")
                     .font(.headline)
                 Spacer()
                 HStack {
                     Spacer()
-                    Button(action: {}) {
-                        Text("새로 고르기")
-                            .padding()
-                            .background(Color.gray)
-                            .foregroundColor(Color.black)
-                            .cornerRadius(10)
-                    }
+
+                    // NavigationLink to NewRoomView
+                    NavigationLink(
+                        destination: NewRoomView(),
+                        isActive: $isShowingNewRoomView,
+                        label: {
+                            Button(action: {
+                                isShowingNewRoomView = true
+                            }) {
+                                Text("새로 고르기")
+                                    .padding()
+                                    .background(Color.gray)
+                                    .foregroundColor(Color.black)
+                                    .cornerRadius(10)
+                            }
+                        }
+                    )
+
                     Spacer()
                     Button(action: {}) {
                         Text("공유하기")
@@ -50,6 +62,7 @@ struct ChoiceView: View {
         }
     }
 }
+
 
 struct ChoiceView_Previews: PreviewProvider {
     static var previews: some View {
